b4865bb5cd2941bdbc117fd5f7d5ff461f2887b6
rasheed <rasheedzein7557@gmail.com>
2019-10-01 14:01:11 +0200

    liste details

--- shlit/src/app/pages/liste/liste-details/liste-details.component.ts
+++ shlit/src/app/pages/liste/liste-details/liste-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Guid } from 'guid-typescript';
+
+import { Liste } from 'src/model/liste';
+import { DataService } from 'src/services/data-service';
 
 @Component({
   selector: 'app-liste-details',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-details.component.scss']
 })
 export class ListeDetailsComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private service: DataService
+  ) {}
 
-  constructor() { }
+  liste: Liste = undefined;
 
   ngOnInit() {
+    var id = this.activatedRoute.snapshot.params.id;
+    var guid = Guid.parse(id);
+    // je demande la liste au service
+    this.service.getListe(guid).then(resultat => {
+      // Quand la liste est là (promesse résolue)
+      // Je la mets dans la propriété liste de mon component
+      this.liste = resultat;
+    });
   }
-
 }
