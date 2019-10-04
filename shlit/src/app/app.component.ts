4f95d72d2a2d3092b20c275dcde906d5571acd75
rasheed <rasheedzein7557@gmail.com>
2019-10-04 11:43:51 +0200

    app emulator battery

--- shlit/src/app/app.component.ts
+++ shlit/src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+import { MessageService } from 'primeng/components/common/messageservice';
+declare const device;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private messageService: MessageService, private zone: NgZone) {}
   applicationName = environment.appName;
+  batteryStatus: string;
+  ngOnInit() {
+    document.addEventListener('deviceready', () => {
+      this.zone.run(() => {
+        this.messageService.add({
+          severity: 'warn',
+          summary: 'Infos Systems',
+          detail: JSON.stringify(device)
+        });
+      });
+
+      window.addEventListener('batterystatus', status => {
+        this.zone.run(() => {
+          this.batteryStatus = JSON.stringify(status);
+        });
+      });
+    });
+  }
 }
--- testProject/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ testProject/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,50 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-battery-status.battery",
+      "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+      "pluginId": "cordova-plugin-battery-status",
+      "clobbers": [
+        "navigator.battery"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device-orientation.CompassError",
+      "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+      "pluginId": "cordova-plugin-device-orientation",
+      "clobbers": [
+        "CompassError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device-orientation.CompassHeading",
+      "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+      "pluginId": "cordova-plugin-device-orientation",
+      "clobbers": [
+        "CompassHeading"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device-orientation.compass",
+      "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+      "pluginId": "cordova-plugin-device-orientation",
+      "clobbers": [
+        "navigator.compass"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-battery-status": "2.0.3",
+    "cordova-plugin-device-orientation": "2.0.1"
   };
 });
