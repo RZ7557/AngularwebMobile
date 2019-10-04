41c4fa91e150d6081a560e0862f3d459f8399295
rasheed <rasheedzein7557@gmail.com>
2019-10-04 16:28:15 +0200

    service http cachestorage

--- shlit/src/services/data-http-cache.service.ts
+++ shlit/src/services/data-http-cache.service.ts
@@ -12,20 +12,33 @@ export class DataHttpCacheService extends DataHttpService {
     super(httpClient);
   }
 
+  getListesFromStorage() {
+    var listeEnJson = localStorage.getItem('listes');
+    var listeObjects = JSON.parse(listeEnJson);
+    var listeListes = listeObjects.map(o => {
+      var l = new Liste(o);
+      l.id = Guid.parse(o.id.value);
+      return l;
+    });
+    return listeListes;
+  }
+
+  dataPeremptionTimeEnS = 5;
+  lastListesRequest: number;
   async getListes(): Promise<Liste[]> {
+    if (
+      localStorage.getItem('listes') &&
+      Date.now() - this.lastListesRequest < this.dataPeremptionTimeEnS * 1000
+    ) {
+      return Promise.resolve(this.getListesFromStorage());
+    }
     try {
       var listes = await super.getListes();
+      this.lastListesRequest = Date.now();
       localStorage.setItem('listes', JSON.stringify(listes));
       return listes;
     } catch (error) {
-      var listeEnJson = localStorage.getItem('listes');
-      var listeObjects = JSON.parse(listeEnJson);
-      var listeListes = listeObjects.map(o => {
-        var l = new Liste(o);
-        l.id = Guid.parse(o.id.value);
-        return l;
-      });
-      return listeListes;
+      return this.getListesFromStorage();
     }
   }
 }
